262375a7329994a406fbae7ef0c26c7c9fd821c1
Chris Peikert <cpeikert@alum.mit.edu>
2021-11-23 17:14:10 -0500

    whitespace, more comments, removed fixed arg from write_salt, buf->coeffs

--- deterministic.c
+++ deterministic.c
@@ -15,15 +15,19 @@ int falcon_det1024_keygen(shake256_context *rng, void *privkey, void *pubkey) {
 		tmpkg, tmpkg_len);
 }
 
+// Domain separator used to construct the fixed versioned salt string.
 uint8_t falcon_det1024_salt_rest[38] = {"FALCON_DET"};
 
-void falcon_det1024_write_salt(uint8_t dst[40], uint8_t salt_version, uint8_t logn) {
+// Construct the fixed salt for a given version.
+void falcon_det1024_write_salt(uint8_t dst[40], uint8_t salt_version) {
 	dst[0] = salt_version;
-	dst[1] = logn;
+	dst[1] = FALCON_DET1024_LOGN;
 	memcpy(dst+2, falcon_det1024_salt_rest, 38);
 }
 
-int falcon_det1024_sign_compressed(void *sig, size_t *sig_len, const void *privkey, const void *data, size_t data_len) {
+int falcon_det1024_sign_compressed(void *sig, size_t *sig_len,
+        const void *privkey, const void *data, size_t data_len) {
+
 	shake256_context detrng;
 	shake256_context hd;
 	size_t tmpsd_len = FALCON_TMPSIZE_SIGNDYN(FALCON_DET1024_LOGN);
@@ -38,16 +42,16 @@ int falcon_det1024_sign_compressed(void *sig, size_t *sig_len, const void *privk
 		return FALCON_ERR_FORMAT;
 	}
 
-	// SHAKE(logn || sk || data)
+	// SHAKE(logn || privkey || data), set to output mode.
 	shake256_init(&detrng);
 	shake256_inject(&detrng, logn, 1);
 	shake256_inject(&detrng, privkey, FALCON_DET1024_PRIVKEY_SIZE);
 	shake256_inject(&detrng, data, data_len);
 	shake256_flip(&detrng);
 
-	falcon_det1024_write_salt(salt, FALCON_DET1024_CURRENT_SALT_VERSION, FALCON_DET1024_LOGN);
+	falcon_det1024_write_salt(salt, FALCON_DET1024_CURRENT_SALT_VERSION);
 
-	// SHAKE(salt || data)
+	// SHAKE(salt || data), still in input mode.
 	shake256_init(&hd);
 	shake256_inject(&hd, salt, 40);
 	shake256_inject(&hd, data, data_len);
@@ -59,6 +63,7 @@ int falcon_det1024_sign_compressed(void *sig, size_t *sig_len, const void *privk
 		return r;
 	}
 
+        // Transform the salted signature to unsalted format.
 	uint8_t *sigbytes = sig;
 	sigbytes[0] = saltedsig[0] | 0x80;
 	sigbytes[1] = FALCON_DET1024_CURRENT_SALT_VERSION;
@@ -69,23 +74,28 @@ int falcon_det1024_sign_compressed(void *sig, size_t *sig_len, const void *privk
 	return 0;
 }
 
-int falcon_det1024_convert_compressed_to_ct(void *sig_ct, const uint8_t *sig_compressed, size_t sig_compressed_len) {
-	int16_t buf[1 << FALCON_DET1024_LOGN];
+int falcon_det1024_convert_compressed_to_ct(void *sig_ct,
+        const void *sig_compressed, size_t sig_compressed_len) {
+
+	int16_t coeffs[1 << FALCON_DET1024_LOGN];
 	size_t v;
 
 	if (((uint8_t*)sig_compressed)[0] != FALCON_DET1024_SIG_COMPRESSED_HEADER) {
 		return FALCON_ERR_BADSIG;
 	}
 
-	v = Zf(comp_decode)(buf, FALCON_DET1024_LOGN, sig_compressed+2, sig_compressed_len-2);
+        // Decode signature's s_bytes into 1024 signed-integer coefficients.
+	v = Zf(comp_decode)(coeffs, FALCON_DET1024_LOGN, sig_compressed+2, sig_compressed_len-2);
 	if (v == 0) {
 		return FALCON_ERR_SIZE;
 	}
 
 	uint8_t *sig = sig_ct;
 	sig[0] = FALCON_DET1024_SIG_CT_HEADER;
-	sig[1] = sig_compressed[1]; // copy the version byte
-	v = Zf(trim_i16_encode)(sig+2, FALCON_DET1024_SIG_CT_SIZE-2, buf, FALCON_DET1024_LOGN,
+	sig[1] = ((uint8_t*)sig_compressed)[1]; // Copy the salt_version byte.
+
+        // Encode the signed-integer coefficients into CT format.
+	v = Zf(trim_i16_encode)(sig+2, FALCON_DET1024_SIG_CT_SIZE-2, coeffs, FALCON_DET1024_LOGN,
 		Zf(max_sig_bits)[FALCON_DET1024_LOGN]);
 	if (v == 0) {
 		return FALCON_ERR_SIZE;
@@ -94,18 +104,23 @@ int falcon_det1024_convert_compressed_to_ct(void *sig_ct, const uint8_t *sig_com
 	return 0;
 }
 
-void falcon_det1024_resalt(uint8_t *salted_sig, const uint8_t *unsalted_sig, size_t unsalted_sig_len) {
+// Construct the corresponding salted signature from an unsalted one.
+void falcon_det1024_resalt(uint8_t *salted_sig,
+        const uint8_t *unsalted_sig, size_t unsalted_sig_len) {
+
 	salted_sig[0] = unsalted_sig[0] & ~0x80; // Reset MSB to 0.
-	uint8_t salt_version = unsalted_sig[1];
-	falcon_det1024_write_salt(salted_sig+1, salt_version, FALCON_DET1024_LOGN);
+	falcon_det1024_write_salt(salted_sig+1, unsalted_sig[1]);
 	memcpy(salted_sig+41, unsalted_sig+2, unsalted_sig_len-2);
 }
 
-int falcon_det1024_verify_compressed(const void *sig, size_t sig_len, const void *pubkey, const void *data, size_t data_len) {
+int falcon_det1024_verify_compressed(const void *sig, size_t sig_len,
+        const void *pubkey, const void *data, size_t data_len) {
+
 	size_t tmpvv_len = FALCON_TMPSIZE_VERIFY(FALCON_DET1024_LOGN);
 	uint8_t tmpvv[tmpvv_len];
 
-	size_t salted_sig_len = sig_len + 40 - 1; // Add back the salt; drop the version byte.
+        // Add back the salt; drop the version byte.
+	size_t salted_sig_len = sig_len + 40 - 1;
 	uint8_t salted_sig[salted_sig_len];
 
 	if (((uint8_t*)sig)[0] != FALCON_DET1024_SIG_COMPRESSED_HEADER) {
@@ -119,7 +134,9 @@ int falcon_det1024_verify_compressed(const void *sig, size_t sig_len, const void
 		tmpvv, tmpvv_len);
 }
 
-int falcon_det1024_verify_ct(const void *sig, const void *pubkey, const void *data, size_t data_len) {
+int falcon_det1024_verify_ct(const void *sig,
+        const void *pubkey, const void *data, size_t data_len) {
+
 	size_t tmpvv_len = FALCON_TMPSIZE_VERIFY(FALCON_DET1024_LOGN);
 	uint8_t tmpvv[tmpvv_len];
 
