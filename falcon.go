b25695d39057402b8254f0e9a9929a482fbb2015
David Lazar <lazard@csail.mit.edu>
2021-12-10 10:37:39 -0500

    Add some Go docs

--- falcon.go
+++ falcon.go
@@ -14,6 +14,8 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with go-algorand.  If not, see <https://www.gnu.org/licenses/>.
 
+// Package falcon implements a deterministic variant of the Falcon
+// signature scheme.
 package falcon
 
 // #cgo CFLAGS: -O3
@@ -31,16 +33,23 @@ const (
 	PublicKeySize = C.FALCON_DET1024_PUBKEY_SIZE
 	// PrivateKeySize is the size of a Falcon private key.
 	PrivateKeySize = C.FALCON_DET1024_PRIVKEY_SIZE
-
+	// CurrentSaltVersion is the salt version number used to compute signatures.
+	// The salt version is incremented when the signing procedure changes (rarely).
 	CurrentSaltVersion = C.FALCON_DET1024_CURRENT_SALT_VERSION
 )
 
 type PublicKey []byte
 type PrivateKey []byte
 
+// CompressedSignature is a deterministic Falcon signature in compressed
+// form, which is variable-length.
 type CompressedSignature []byte
+
+// CTSignature is a deterministic Falcon signature in constant-time form,
+// which is fixed-length.
 type CTSignature []byte
 
+// GenerateKey generates a public/private key pair from the given seed.
 func GenerateKey(seed []byte) (PublicKey, PrivateKey, error) {
 	var rng C.shake256_context
 	C.shake256_init_prng_from_seed(&rng, unsafe.Pointer(&seed[0]), C.size_t(len(seed)))
@@ -56,6 +65,8 @@ func GenerateKey(seed []byte) (PublicKey, PrivateKey, error) {
 	return publicKey, privateKey, nil
 }
 
+// SignCompressed signs the message with privateKey and returns a compressed
+// signature, or an error if signing fails (e.g., due to a malformed private key).
 func SignCompressed(privateKey PrivateKey, msg []byte) (CompressedSignature, error) {
 	data := C.NULL
 	if len(msg) > 0 {
@@ -71,6 +82,7 @@ func SignCompressed(privateKey PrivateKey, msg []byte) (CompressedSignature, err
 	return sig, nil
 }
 
+// ConvertToCT converts a compressed signature to a CT signature.
 func (sig CompressedSignature) ConvertToCT() (CTSignature, error) {
 	sigCT := make([]byte, C.FALCON_DET1024_SIG_CT_SIZE)
 	r := C.falcon_det1024_convert_compressed_to_ct(unsafe.Pointer(&sigCT[0]), unsafe.Pointer(&sig[0]), C.size_t(len(sig)))
@@ -80,6 +92,7 @@ func (sig CompressedSignature) ConvertToCT() (CTSignature, error) {
 	return sigCT, nil
 }
 
+// Verify reports whether sig is a valid signature of msg by publicKey.
 func (sig CompressedSignature) Verify(publicKey PublicKey, msg []byte) bool {
 	data := C.NULL
 	if len(msg) > 0 {
@@ -89,6 +102,7 @@ func (sig CompressedSignature) Verify(publicKey PublicKey, msg []byte) bool {
 	return r == 0
 }
 
+// Verify reports whether sig is a valid signature of message by publicKey.
 func (sig CTSignature) Verify(publicKey PublicKey, msg []byte) bool {
 	data := C.NULL
 	if len(msg) > 0 {
@@ -98,10 +112,12 @@ func (sig CTSignature) Verify(publicKey PublicKey, msg []byte) bool {
 	return r == 0
 }
 
+// SaltVersion returns the salt version number used to compute the signature.
 func (sig CompressedSignature) SaltVersion() int {
 	return int(sig[1])
 }
 
+// SaltVersion returns the salt version number used to compute the signature.
 func (sig CTSignature) SaltVersion() int {
 	return int(sig[1])
 }
